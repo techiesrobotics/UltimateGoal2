0e72c4041c6c607ae63fabf3108bc83a86c22462
tinyandblaze <tinyandblaze@gmail.com>
2021-07-29 15:18:45 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechiesAutoWithCameraDection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechiesAutoWithCameraDection.java
@@ -53,6 +53,7 @@ import java.util.List;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
+// nathan test
 @Autonomous(name = "TechiesAuto:WithCamera", group = "Concept")
 // @Disabled
 public class TechiesAutoWithCameraDection extends LinearOpMode {
