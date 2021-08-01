348123616734b2bde8fc7ea214220612992b9d3c
karenbingfengli <karenbingfengli@gmail.com>
2021-08-01 14:34:37 -0400

    added easy open cv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoC.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
+@Disabled
 @Autonomous(name= "XXX Techies Autonomous C", group="Pushbot")
 public class AutoC extends TechiesAutonomous {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyOpMode.java
@@ -16,7 +16,7 @@ public class MyOpMode extends LinearOpMode {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         // We want to start the bot at x: 10, y: -8, heading: 90 degrees
-       Pose2d startPose = new Pose2d(5,-8,Math.toRadians(90));//new Pose2d(5, -8, Math.toRadians(90));KL change
+       Pose2d startPose = new Pose2d(72,-26,Math.toRadians(0));//new Pose2d(5, -8, Math.toRadians(90));KL change
 
        drive.setPoseEstimate(startPose);
 /*
@@ -31,13 +31,15 @@ public class MyOpMode extends LinearOpMode {
         Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
                 .forward(5)
                 .build();
+                A- 15,-40
+                B- -10, -5
 */
 
         Trajectory traj1 = drive.trajectoryBuilder(startPose) // kl change
-                .strafeRight(10)
+                .strafeTo(new Vector2d(-35, -40))
                 .build();
 
-        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
+      /*  Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
                 .splineTo(new Vector2d(5, 6), 0)
                 .splineTo(new Vector2d(9, -10), 0)
                 .build();
@@ -45,44 +47,15 @@ public class MyOpMode extends LinearOpMode {
         Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
                 .splineTo(new Vector2d(5, 6), 0)
                 .splineTo(new Vector2d(9, -10), 0)
-                .build();
+                .build(); */
 
         waitForStart();
 
         if(isStopRequested()) return;
 
         drive.followTrajectory(traj1);
-        drive.followTrajectory(traj2);
-        drive.followTrajectory(traj3);
+        //drive.followTrajectory(traj2);
+        //drive.followTrajectory(traj3);
 
-      /*  Trajectory traj1 = drive.trajectoryBuilder(startPose)
-                .splineTo(new Vector2d(20, 9), Math.toRadians(45))
-                .build();
-
-       Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
-                .splineTo(new Vector2d(20, 9), Math.toRadians(45))
-                .build();
-
-        waitForStart();
-
-        if(isStopRequested()) return;
-
-        drive.followTrajectory(traj1);   */
-       // drive.followTrajectory(traj2);
-
-/*
-        Pose2d startPose = new Pose2d(62, -22, Math.toRadians(180));
-
-        drive.setPoseEstimate(startPose);
-
-        Trajectory traj1 = drive.trajectoryBuilder(new Pose2d())
-                .strafeTo(new Vector2d(0, 0))
-                .build();
-
-        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
-                .forward(5)
-                .build();
-
-       */
     }
 }
